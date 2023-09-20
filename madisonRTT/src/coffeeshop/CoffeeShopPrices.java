e0453f7a89708fddfc4d5fd28ee99fd717cda789
Madison Buck <121197013+MadisonProjects@users.noreply.github.com>
2023-09-20 15:53:54 -0500

    ☕️ more updates Sep 20th

--- madisonRTT/src/coffeeshop/CoffeeShopPrices.java
+++ madisonRTT/src/coffeeshop/CoffeeShopPrices.java
@@ -3,11 +3,17 @@ package coffeeshop;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CoffeeShopPrices {
 	
+	// Menu
 	
 	List<Product> products = new ArrayList<> ();
+	
+	// class member variable
+	
+	Scanner input = new Scanner(System.in);
 	
 	
 	public void setupProducts() {
@@ -26,15 +32,17 @@ public class CoffeeShopPrices {
 		cookie.setPrice(6.77);
 		products.add(cookie);
 		
-		printAllProducts();
+		Product sandwich = new Product ();
+		sandwich.setName("Egg & Cheese Muffin");
+		sandwich.setPrice(9.99);
+		products.add(sandwich);
+				
+	
 		
 	}
-	
 	public void printProduct(Product product) {
-		System.out.println("Product name : " + 
-				product.getName() + " Price : " 
-				+ product.getPrice () ) ;
-		
+ 		System.out.println("Product name : " + product.getName() + " Price : " + product.getPrice());
+ 		
 	}
 	
 	public void printAllProducts() {
@@ -43,9 +51,15 @@ public class CoffeeShopPrices {
 		}
 	}
 	
-	public static void main(String[] args) {
-		CoffeeShopPrices cf = new CoffeeShopPrices();
-		cf.setupProducts();
+		
+
+	// move this later: public static void main(String[] args) {
+		// CoffeeShopPrices cf = new CoffeeShopPrices();
+		// cf.setupProducts();
+	
+	public void example() {
+		
+		
 	
 		// the value in each product is the price
 		double coffee = 5.44d;
@@ -63,16 +77,43 @@ public class CoffeeShopPrices {
 		// 2 items of the 3rd product
 		subTotal = subTotal + (cookie * 1);
 		
-		DecimalFormat df = new DecimalFormat ("$0.00");
-		System.out.println("subtotal : " + df.format(subTotal));
+		DecimalFormat df = new DecimalFormat ("$#,##0.00");
+		System.out.println("Subtotal " + df.format(subTotal));
 		
 		double salesTax = subTotal * 0.10;
-		System.out.println("Sales Tax : " + df.format(salesTax));
+		System.out.println("Sales Tax\t : " + df.format(salesTax));
 		
 		double totalSale = subTotal + salesTax;
-		System.out.println("Total  : " + df.format(totalSale));
-		
+		System.out.println("Total\t\t" + df.format(totalSale));	
 		
 	}
 
+		public int userSelect( ) {
+			System.out.println("1) Print the menu items and prices") ;
+			System.out.println("2) Add an item to your order") ;
+			System.out.println("3) Print the items in your order.") ;
+			System.out.println("4) Checkout") ;
+			
+			System.out.print ("What would you like to do?") ;
+		
+			
+			int select = input.nextInt();
+			
+			return select;
+		} 
+		
+		public static void main(String[] args) {
+			CoffeeShopPrices cf = new CoffeeShopPrices();
+			cf.setupProducts();
+			
+			int userSelection = cf.userSelect();
+			if (userSelection == 1) {
+				cf.printAllProducts();
+			} else {
+					System.out.println("User input " + userSelection + "is unknown.  Try again;");
+			}
+	  }
 }
+
+		
+		

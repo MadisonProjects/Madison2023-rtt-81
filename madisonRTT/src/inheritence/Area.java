5bb5f984fb8e8629e6ada6b0539782bd15eb1b45
Madison Buck <121197013+MadisonProjects@users.noreply.github.com>
2023-09-30 12:51:47 -0500

    Friday

---
+++ madisonRTT/src/inheritence/Area.java
@@ -0,0 +1,7 @@
+package inheritence;
+
+public interface Area {
+
+	public double calculateArea();
+
+}
---
+++ madisonRTT/src/inheritence/Shape.java
@@ -0,0 +1,14 @@
+package inheritence;
+
+public abstract class Shape implements Area {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName (String name) {
+		this.name = name;
+	}
+}

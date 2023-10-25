de67c5a84459e75e3fa594769c00c872d3175283
Madison Buck <madisonsbuck@gmail.com>
2023-10-25 13:48:32 -0500

    PreparedStatementExample Cont

--- databases/src/main/java/CreateOrderDetails.java
+++ databases/src/main/java/CreateOrderDetails.java
@@ -1,22 +1,39 @@
-package org.perschola.database;
+package org.perscholas.database;
 
+import org.perscholas.database.dao.OrderDAO;
+import org.perscholas.database.dao.OrderDetailsDAO;
+import org.perscholas.database.dao.ProductDAO;
 import org.perscholas.database.entity.Order;
 import org.perscholas.database.entity.OrderDetail;
 import org.perscholas.database.entity.Product;
 
 public class CreateOrderDetails {
 
-    private OrderDAO orderDAO =  new OrderDAO ();
-    private ProductDAO productDAO = new ProductDAO ();
+    private OrderDAO orderDAO = new OrderDAO();
+    private ProductDAO productDAO = new ProductDAO();
     private OrderDetailsDAO orderDetailsDAO = new OrderDetailsDAO();
+
 
     public void createOrderDetails() {
         // the goal is to insert a record into the order details table
         Product p = productDAO.findById(1);
         Order o = orderDAO.findById(10100);
 
-        OrderDetail of = new OrderDetail();
+        OrderDetail od = new OrderDetail();
         od.setProduct(p);
-        od.detOrder
+        od.setOrder(o);
+        od.setOrderLineNumber(200);
+        od.setPriceEach(5.55);
+        od.setQuantutyOrdered(300);
+
+        orderDetailsDAO.save(od);
     }
+
+
+    public static void main(String[] args) {
+        CreateOrderDetails cod = new CreateOrderDetails();
+        cod.createOrderDetails();
+    }
+
+
 }
